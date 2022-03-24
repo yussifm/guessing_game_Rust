e5aefe01969ff7dc32ef3f99b3700b210cd7eebf
yussifm13 <yussifm13@gmail.com>
2022-03-24 02:19:01 +0000

    ini

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
+use rand::Rng;
 use std::io;
-use rand::Rng
 
 fn main() {
     println!("Guess a number!");
     let secret_num = rand::thread_rng().gen_range(1..101);
-    println!("Please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
 
     println!("you guessed: {}", guess);
+
+    loop {
+        println!("Please input your guess.");
+
+        match guess.cmp(&secret_num) {
+            std::cmp::Ordering::Less => println!("Too small"),
+            std::cmp::Ordering::Equal => {
+                print!("You win!!!");
+                break;
+            }
+            std::cmp::Ordering::Greater => println!("Too Big"),
+        }
+    }
 }
