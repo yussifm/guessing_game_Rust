5f0cd997627ba803d34a268c301c5f861f47550a
yussifm13 <yussifm13@gmail.com>
2022-03-24 02:38:47 +0000

    fininsh

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,19 @@ fn main() {
     println!("Guess a number!");
     let secret_num = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };
-
-    println!("you guessed: {}", guess);
-
+    
     loop {
         println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed: {}", guess);
 
         match guess.cmp(&secret_num) {
             std::cmp::Ordering::Less => println!("Too small"),
